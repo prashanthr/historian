31fb421ab5491e25d5c5db24681a52b242e2b88e
Prashanth R <1409501+prashanthr@users.noreply.github.com>
2020-12-17 18:27:16 -0800

    write to file

--- src/server/test-history.js
+++ src/server/test-history.js
@@ -2,6 +2,11 @@
 import u from 'unfurled'
 import axios from 'axios'
 import { sortBy } from 'lodash'
+import fs from 'fs'
+import path from 'path'
+
+const fsp = fs.promises
+
 const unfurly = async ({ url }) => {
   console.log(`Unfurling ${url}...`)
   const result = await u(url, { oembed: true }) //unfurl({ url })
@@ -50,8 +55,8 @@ const combineHistoryByDate = ({ data, date, url }) => {
   const { Events, Births, Deaths } = data
   const combined = [ 
     ...Events.map(i => ({ ...i, type: 'event' })), 
-    ...Births.map(i => ({ ...i, type: 'birth' })), 
-    ...Deaths.map(i => ({ ...i, type: 'death' }))
+    // ...Births.map(i => ({ ...i, type: 'birth' })), 
+    // ...Deaths.map(i => ({ ...i, type: 'death' }))
   ]
   return {
     date,
@@ -60,15 +65,25 @@ const combineHistoryByDate = ({ data, date, url }) => {
   }
 }
 
+const writeToFileJSON = async ({ 
+  data
+}) => {
+  const outputFile = path.resolve(__dirname, '../data/history.json')
+  console.log('Writing to file')
+  await fsp.writeFile(outputFile, JSON.stringify(data, 0, 2))
+}
+
 const test = async () => {
   const date = new Date()
   // const historyUrl = 'https://wikipedia.org/wiki/December_13'
   const historyData = await history({ month: date.getMonth() + 1, day: date.getDate() })
   const combinedHistory = combineHistoryByDate(historyData)
   const items = combinedHistory.items//.slice(0, 10)
+  let result = []
   for (const item of items) {
-    await unfurlHistoryItem(item)
+    result.push(await unfurlHistoryItem(item))
   }
+  await writeToFileJSON({ data: result })
 }
 
 test().catch(err => console.log('Error running test', err))
