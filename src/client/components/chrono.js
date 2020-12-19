2f7a753d78dc67aafa3ed67de64c62eb45041cde
Prashanth R <1409501+prashanthr@users.noreply.github.com>
2020-12-18 16:31:08 -0800

    use sample data to see it on the UI

--- src/client/components/chrono.js
+++ src/client/components/chrono.js
@@ -1,28 +1,41 @@
 import React from 'react'
 import { Chrono } from 'react-chrono'
-
-const Chronoo = ({ items, slideshow , mode }) => (
+import SAMPLE_DATA from '../../data/history.json'
+const Chronoo = ({ items, slideshow , mode, scrollable }) => (
   <Chrono 
     items={items} 
     slideShow={slideshow}
     mode={mode}
+    scrollable={scrollable}
   />
 )
 
 Chronoo.defaultProps = {
   mode: 'VERTICAL_ALTERNATING',
+  scrollable: true,
   slideshow: false,
-  items: [{
-    title: "May 1940",
-    cardTitle: "Dunkirk",
-    cardSubtitle:"Men of the British Expeditionary Force (BEF) wade out to..",
+  items: SAMPLE_DATA.map(i => ({
+    title: i.year,
+    cardTitle: i.links && i.links.length > 0 ? i.links[0].title : `${i.text.substr(0, 50)}...`,
+    cardSubtitle: i.text,
     media: {
       type: "IMAGE",
       source: {
-        url: "http://someurl/image.jpg"
+        url: i.unfurlData && i.unfurlData.media.imageUrls && i.unfurlData.media.imageUrls.length > 0 ? i.unfurlData.media.imageUrls[0] : ''
       }
     }
-  }]
+  }))
+  // items: [{
+  //   title: "May 1940",
+  //   cardTitle: "Dunkirk",
+  //   cardSubtitle:"Men of the British Expeditionary Force (BEF) wade out to..",
+  //   media: {
+  //     type: "IMAGE",
+  //     source: {
+  //       url: "http://someurl/image.jpg"
+  //     }
+  //   }
+  // }]
 }
 
 export default Chronoo
